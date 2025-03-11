f351f40a1d20ecfb9f14c497f747fc5aa1bd9384
SV-jtv <yoselyn.varela.7e6@itb.cat>
2025-03-11 14:00:24 +0100

    Primera_app-SV

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -6,13 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,27 +25,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             HelloWorldTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Exercici1(modifier = Modifier.padding(innerPadding))
+                    Exercici4(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    //Text(text = "Hello $name!",modifier = modifier)
+//Text(text = "Hello $name!",modifier = modifier)
 
-    /*Column() {
-        Text("Text line 1", Modifier.background(Color.Cyan).weight(1f))
-        Text("Text line 2", Modifier.background(Color.Yellow).weight(1f))
-        Text("Text line 3", Modifier.background(Color.Green).weight(1f))
-        Text("Text line 4", Modifier.background(Color.Magenta).weight(1f).fillMaxWidth( ))
-        Text("Text line 5", Modifier.background(Color.Gray).weight(1f))
-    }*/
+/*Column() {
+    Text("Text line 1", Modifier.background(Color.Cyan).weight(1f))
+    Text("Text line 2", Modifier.background(Color.Yellow).weight(1f))
+    Text("Text line 3", Modifier.background(Color.Green).weight(1f))
+    Text("Text line 4", Modifier.background(Color.Magenta).weight(1f).fillMaxWidth( ))
+    Text("Text line 5", Modifier.background(Color.Gray).weight(1f))
+}*/
+
+@Composable
+fun Exercici1(modifier: Modifier = Modifier) {
     ConstraintLayout (modifier.fillMaxSize()) {
         val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
         Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
@@ -59,28 +55,57 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         })
         Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
             bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.start)
+            end.linkTo(boxRed.start)
         })
         Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
             top.linkTo(boxRed.bottom)
-            start.linkTo(boxRed.start)
+            start.linkTo(boxRed.end)
         })
         Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
-            top.linkTo(boxRed.top)
+            top.linkTo(boxRed.bottom)
             end.linkTo(boxRed.start)
         })
         Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
-            bottom.linkTo(boxRed.bottom)
+            bottom.linkTo(boxRed.top)
             start.linkTo(boxRed.end)
         })
     }
-    Text(text = "Bye $name!",modifier = modifier)
+}
+
+@Composable
+fun Exercici4(modifier: Modifier = Modifier) {
+    ConstraintLayout (modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
+        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
+            bottom.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
+            top.linkTo(boxRed.bottom)
+            start.linkTo(boxRed.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        Exercici1()
+        //Exercici4()
     }
 }
